af2d0bd05089cb54103f31f689f17f87d1d2b604
mishalz <mishal.zulfiqar2911@gmail.com>
2023-11-24 13:58:06 +0000

    Added Post, User and Comment models, retry

--- models/Post.js
+++ models/Post.js
@@ -1,18 +1,5 @@
 const mongoose = require("mongoose");
 
-/* Post Model
-    -- title: String
-    -- topic: String [] (from a set: {Politics, Health, Sport or Tech}) 
-    -- message: String
-    -- author: User model
-    -- likes: [{
-        userId: user object
-        username: String
-    }]
-    -- comments: [comment object ids]
-    -- timestamp: Date.now()
-    -- expiration: Date */
-
 const postSchema = mongoose.Schema(
   {
     title: {
--- models/User.js
+++ models/User.js
@@ -1,8 +1,5 @@
 const mongoose = require("mongoose");
-/* User Model
-    -- username: String
-    -- email: String
-    -- password: String */
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
